4699e8f2a239fda724132c499e37ab005848e215
Xin Zhou <xin.zhou@monash.edu>
2024-04-19 15:13:32 +1000

    update data in script

--- scripts/delete_cache.sh
+++ scripts/delete_cache.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# delete unnecessary files
 find . -name "*.pyc" | xargs rm -rf
 find . -type d -name "__pycache__" -exec rm -rf {} +
 find . -type d -name ".DS_Store" -exec rm -rf {} +
+
+# all changes are added to the git
 git add .
 git commit -m "update data in script"
+git config --global push.default simple
 git push
